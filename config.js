d0999e9fb9e935e5bbf0180490d7573cdb09f9cd
netccusa <ppaydaydocuments@gmail.com>
2025-12-22 20:18:02 +1000

    Small Admin and Version Patches

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyBg3KRIIvzkTA8OnrEBsln-aPcjU9DrBA4",
     authDomain: "netccusa.firebaseapp.com",
     projectId: "netccusa",
-    version: "-alpha, created by pPayday"
+    version: "-1.0 | Created by pPayday. | Last Updated: 22/12/25"
 };
 
 export default firebaseConfig;

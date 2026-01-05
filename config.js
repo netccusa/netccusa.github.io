f091a1f0a1499f01bcd8b8e63b05ff1a74d41099
netccusa <ppaydaydocuments@gmail.com>
2026-01-05 12:52:54 +1000

    Updated Version Footers and Dates

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyBg3KRIIvzkTA8OnrEBsln-aPcjU9DrBA4",
     authDomain: "netccusa.firebaseapp.com",
     projectId: "netccusa",
-    version: "-1.0 | Created by pPayday. | Last Updated: 22/12/25"
+    version: "-1.1 | Created by pPayday. | Last Updated: 05/01/26"
 };
 
 export default firebaseConfig;
